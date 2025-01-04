0eb5c14c615e7098de62091e6999ae1886c2b468
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2025-01-05 00:29:16 +0530

    update login.jsx

--- client/src/user/Login.jsx
+++ client/src/user/Login.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import api from "../redux/api";
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
