94becf81df460c744e813b16b390578860ac212d
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2025-01-16 23:30:56 +0530

    minor bugs

--- client/src/user/SignUp.jsx
+++ client/src/user/SignUp.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../redux/api";
 import { useSnackbar } from "notistack";
-import { Link } from "react-router-dom";
 
 const Signup = () => {
   const navigate = useNavigate();
